64003316d56c07d34d13704d6cfeaadcac524a31
Fery <ferywardiyanto@gmail.com>
2016-10-02 04:32:00 +0700

    Update installation output
    Signed-off-by: Fery <ferywardiyanto@gmail.com>

--- install.sh
+++ install.sh
@@ -7,26 +7,31 @@ set -e
 # fg: 31 red,  32 green, 33 yellow, 34 blue, 35 purple, 36 cyan, 37 white
 # bg: 40 black, 41 red, 44 blue, 45 purple
 e() {
-    printf '\033[%sm%s\033[m\n' "$@"
+    printf '\033[%sm%s\033[m' "$@"
 }
 
 my_pwd="$PWD"
 now=`date +'%Y-%m-%d'`
 backup_dir="${my_pwd}/dotfiles.old/${now}"
 dotfiles="aliases profile bash_prompt bashrc exports functions vimrc"
+requirements="git curl vim"
+
+for program in $requirements; do
+    if ! command -v $program >/dev/null 2>&1; then
+        e '31' "We need ${program} but it's not installed."
+        exit 1
+    fi
+done
+
+e '33' 'Setup dotfiles'
 
 cd $HOME
-
-if [ ! -f $HOME/.gitconfig ]; then
-    cp -f $my_pwd/.gitconfig $HOME/.gitconfig
-fi
 
 for dotfile in $dotfiles; do
     [ ! -d "$backup_dir" ] && mkdir -p $backup_dir
 
     if [ -f "$HOME/.$dotfile" ]; then
         mv "$HOME/.$dotfile" "$backup_dir/.$dotfile"
-        e '33' "Backing up your .$dotfile"
     fi
 
     ln -s "$my_pwd/.$dotfile" .
@@ -36,28 +41,47 @@ for dotfile in $dotfiles; do
         ln -s .profile .bash_profile
     fi
 done
-unset dotfile
+unset dotfile dorfiles
+
+e '32' 'Ready\n'
 
 if command -v zsh >/dev/null 2>&1; then
-    e '33' 'Begin Installing Oh-my-ZSH'
+    e '33' 'Setup Oh-my-ZSH'
     [ -d ~/.oh-my-zsh ] || curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh -
     [ -f ~/.oh-my-zsh/themes/honukai.zsh-theme ] && rm ~/.oh-my-zsh/themes/honukai.zsh-theme
     ln -s  $my_pwd/.zsh-themes/honukai.zsh-theme ~/.oh-my-zsh/themes/
     [ -f ~/.zshrc ] && mv ~/.zshrc $backup_dir/
     ln -s $my_pwd/.zshrc ~/.zshrc
+    e '32' 'Ready\n'
 fi
 
 
 if command -v tmux >/dev/null 2>&1; then
-    e '33' 'Begin setup your TMUX'
+    e '33' 'Setup TMUX'
     [ -f ~/.tmux.conf ] && mv ~/.tmux.conf $backup_dir/
     ln -s $my_pwd/.tmux.conf ~/.tmux.conf
+    e '32' 'Ready\n'
 fi
 
 source ~/.bashrc
 
-e '32' 'Your dotfiles are ready to use!'
-e '33' 'Begin installing Powerline Fonts!'
+e '33' 'Setup gitconfig!'
+  
+if [ -f $HOME/.gitconfig ]; then
+    git_email=`git config --global user.email`
+    git_name=`git config --global user.name`
+    mv ~/.gitconfig $backup_dir/
+fi
+
+cp -f $my_pwd/.gitconfig ~/.gitconfig
+
+if [ ! -z "$git_email" ] && [ ! -z "$git_name" ]; then
+    git config --global user.email $git_email
+    git config --global user.name $git_name
+fi
+
+e '32' 'Ready\n'
+e '33' 'Setup Powerline Fonts!'
 
 [ ! -d ~/.local/share/fonts ] && mkdir -p ~/.local/share/fonts
 curl -LSso ~/.local/share/fonts/PowerlineSymbols.otf https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
@@ -66,8 +90,8 @@ command -v fc-cache >/dev/null 2>&1 && fc-cache -f ~/.local/share/fonts
 [ ! -d ~/.config/fontconfig/conf.d ] && mkdir -p ~/.config/fontconfig/conf.d
 curl -LSso ~/.config/fontconfig/conf.d/10-powerline-symbols.conf https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
 
-e '32' 'Your Powerline Fonts are installed'
-e '33' 'Begin setup your VIM'
+e '32' 'Ready\n'
+e '33' 'Setup VIM'
 
 [ -d ~/.vim ] && mv ~/.vim $backup_dir/.vim
 mkdir ~/.vim && cd ~/.vim
@@ -78,13 +102,14 @@ for vim_dir in $vim_dirs; do
 done
 unset vim_dir vim_dirs
 
-e "33" "Begin installing pathogen"
-mkdir -p autoload bundle && \
-curl -LSso autoload/pathogen.vim https://tpo.pe/pathogen.vim && \
-git init && \
-git a && git c "Initial commit && install autoload/pathogen"
+e "33" '- Setup Pathogen'
 
-e "33" "Begin installing VIM plugins"
+mkdir -p autoload bundle && curl -LSso autoload/pathogen.vim https://tpo.pe/pathogen.vim && \
+git init && git a && git c "Initial commit && install autoload/pathogen" > /dev/null
+
+e '32' 'Ready\n'
+
+e '33' '- VIM plugins'
 declare -A plugins
 plugins[mattn/emmet-vim]=bundle/emmet
 plugins[scrooloose/nerdtree]=bundle/nerdtree
@@ -102,25 +127,17 @@ plugins[vim-airline/vim-airline-themes]=bundle/vim-airline-themes
 
 for repo in ${!plugins[@]}; do
     plugin=${plugins[$repo]}
-    git submodule -q add github:$repo $plugin && git a && git c "~/.vim/$plugin plugin installed"
+    message="$plugin plugin is installed"
+    git submodule -q add github:$repo $plugin
+    git a && git c "$message" > /dev/null
+    e '33' "   - $message"
 done
-unset repo plugin
+unset repo plugin message
 
-me() {
-    git_name="$(git config --global user.name)"
-    git_email="$(git config --global user.email)"
-    [[ -n "$git_name" && -n "$git_email" ]] && echo "$git_name <$git_email>"
-}
+e '32' 'All of your VIM plugins are installed'
 
 cd $my_pwd
-e "32" "Everything's done!"
+e "32" "Everything's done! your dotfiles is ready to use"
 e "32" "Your old files are backed up in $backup_dir"
+e "32" "Thank you $git_name <$git_email>"
 
-if [[ -n "$(me)" ]]; then
-    e "32" "Thank you $(me)"
-    unset git_name git_email
-else
-    e "33" "Don't forget to setup your git user.name and user.email, Please run"
-    e "33" ' $ git config --global user.name <your name>'
-    e "33" ' $ git config --global user.email <your email>'
-fi
