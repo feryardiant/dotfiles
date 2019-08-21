4d9f74cefdd6238816a0de087df442f6cabaf47c
Fery Wardiyanto <ferywardiyanto@gmail.com>
2019-08-22 05:59:03 +0700

    Use vim-nox for python3 support. See: https://askubuntu.com/a/790254/10706
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- scripts/vim.sh
+++ scripts/vim.sh
@@ -6,9 +6,9 @@
 	# run inside sudo
 	sudo sh <<SCRIPT
 	if ! command -v vim >/dev/null 2>&1; then
-		apt-get install vim -yqq
+		apt-get install vim-nox -yqq
 	else
-		apt-get upgrade vim -yqq
+		apt-get upgrade vim-nox -yqq
 	fi
 SCRIPT
 }
