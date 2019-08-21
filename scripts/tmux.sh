65ef7e5a5c9fceedf5dd278d7d540d0dedbb3de7
Fery Wardiyanto <ferywardiyanto@gmail.com>
2019-08-22 04:05:43 +0700

    Revert back to 334c84d
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- scripts/tmux.sh
+++ scripts/tmux.sh
@@ -5,24 +5,10 @@
 
 	# run inside sudo
 	sudo sh <<SCRIPT
-	if [ "$(lsb_release -cs)" = 'xenial' ]; then
-		if command -v tmux >/dev/null 2>&1; then
-			apt-get purge tmux -yqq
-		fi
-
-		if ! command -v tmux-next >/dev/null 2>&1; then
-			apt-get install tmux-next -yqq
-		else
-			apt-get upgrade tmux-next -yqq
-		fi
-
-		sudo update-alternatives --install /usr/bin/tmux tmux /usr/bin/tmux-next 60
+	if ! command -v tmux >/dev/null 2>&1; then
+		apt-get install tmux -yqq
 	else
-		if ! command -v tmux >/dev/null 2>&1; then
-			apt-get install tmux -yqq
-		else
-			apt-get upgrade tmux -yqq
-		fi
+		apt-get upgrade tmux -yqq
 	fi
 SCRIPT
 }
