8ad41fe037883903700b66478bf1bf198a60d32b
Fery Wardiyanto <ferywardiyanto@gmail.com>
2019-08-23 03:22:34 +0700

    Update initializer
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,23 +5,32 @@
 
 	# run inside sudo
 	sudo sh <<SCRIPT
-	locale-gen $LANG
+	printf ' - Localization...'
+	locale-gen $LANG > /dev/null 2>&1
 	update-locale LC_ALL="$LANG" LANG="$LANG"
-	dpkg-reconfigure --frontend noninteractive locales
+	dpkg-reconfigure --frontend noninteractive locales > /dev/null 2>&1
+	printf ' done\n'
 
+	printf ' - Add repositories...'
 	# Latest GIT
-	add-apt-repository ppa:git-core/ppa -y
+	add-apt-repository ppa:git-core/ppa -y > /dev/null 2>&1
 
 	# Latest TMUX & VIM
-	add-apt-repository ppa:pi-rho/dev -y
+	add-apt-repository ppa:pi-rho/dev -y > /dev/null 2>&1
 
 	# Latest NeoVIM
-	add-apt-repository ppa:neovim-ppa/stable -y
+	add-apt-repository ppa:neovim-ppa/stable -y > /dev/null 2>&1
+	printf ' done\n'
 
 	# Apply changes
-	apt-key update && apt-get update
+	printf ' - Update repositories...'
+	apt-key update > /dev/null 2>&1
+	apt-get update -qq
+	printf ' done\n'
 
 	# Update
-	apt-get install python-pip python3-pip -y
+	printf ' - Install dependencies...'
+	apt-get install python-pip python3-pip -y > /dev/null
+	printf ' done\n'
 SCRIPT
 }
