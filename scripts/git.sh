ed11bb38a9c500364d0698234b6f9b02567346b7
Fery Wardiyanto <ferywardiyanto@gmail.com>
2022-01-10 06:01:30 +0700

    chore: add git-core ppa
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- scripts/git.sh
+++ scripts/git.sh
@@ -5,6 +5,9 @@ sudo -k
 
 # run inside sudo
 sudo sh <<SCRIPT
+# Latest GIT
+add-apt-repository ppa:git-core/ppa -y > /dev/null 2>&1
+
 if ! command -v git >/dev/null 2>&1; then
 	apt-get install git -yqq
 else
