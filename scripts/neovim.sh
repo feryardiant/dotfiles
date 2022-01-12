dc9b9c1a8291a9f6002cc3c6415a28ddae2815e9
Fery Wardiyanto <ferywardiyanto@gmail.com>
2022-01-12 09:36:39 +0700

    chore: add ppa to neovim script
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- scripts/neovim.sh
+++ scripts/neovim.sh
@@ -5,6 +5,8 @@ sudo -k
 
 # run inside sudo
 sudo sh <<SCRIPT
+add-apt-repository ppa:neovim-ppa/stable -y > /dev/null 2>&1
+
 if ! command -v nvim >/dev/null 2>&1; then
 	apt-get install neovim -yqq
 fi
