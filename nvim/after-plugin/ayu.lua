a63c5b5d57c67554b4f674b937f6a541f83fb422
Fery Wardiyanto <ferywardiyanto@gmail.com>
2023-08-29 19:17:33 +0700

    chore(nvim): give ayu theme a touch
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- nvim/after-plugin/ayu.lua
+++ nvim/after-plugin/ayu.lua
@@ -8,7 +8,15 @@ colors.generate()
 ayu.setup({
     overrides = function()
         return {
-            Comment = { fg = colors.comment }
+            Normal = { bg = 'none' },
+            NormalFloat = { bg = '#3a3a3a' },
+            SignColumn = { bg = 'none' },
+            Comment = { fg = colors.comment },
+            CursorLine = { bg = '#1c1c1c' },
+            CursorLineNr = { bg = '#1c1c1c', fg = colors.accent },
+            ColorColumn = { bg = '#262626' },
+            Pmenu = { bg = '#303030' },
+            PmenuSel = { bg = '#3a3a3a' }
         }
     end
 })
