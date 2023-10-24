b0081b3304f01a28fe4f4dcd20c6b1e2f5bd80dd
Fery Wardiyanto <ferywardiyanto@gmail.com>
2023-10-25 02:50:34 +0700

    chore(nvim): borrow some base config from LazyVim
    see https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
    
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,9 +7,16 @@ require('lazy').setup({
   spec = { import = 'plugins' },
 })
 
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.completeopt = 'menu,menuone,noselect'
+vim.opt.conceallevel = 3
+vim.opt.confirm = true
+
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
+
+vim.opt.scrolloff = 5
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
@@ -18,16 +25,37 @@ vim.opt.expandtab = true
 
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-
-vim.opt.wrap = false
+vim.opt.smartindent = true
 
 vim.opt.list = true
 vim.opt.listchars = 'tab:→ ,trail:·,extends:…,precedes:…'
+vim.opt.fillchars = {
+  foldopen = "",
+  foldclose = "",
+  -- fold = "⸱",
+  fold = " ",
+  foldsep = " ",
+  diff = "╱",
+  eob = " ",
+}
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.undofile = true
+vim.opt.undolevels = 10000
 
 vim.opt.showmode = false
 vim.opt.termguicolors = true
 vim.opt.colorcolumn = '80,100,120'
 vim.opt.updatetime = 50
 
-vim.opt.hlsearch = true
-vim.opt.incsearch = true
+vim.opt.wildmode = 'longest:full,full'
+vim.opt.wrap = false
+
+if vim.fn.has("nvim-0.10") == 1 then
+  vim.opt.smoothscroll = true
+  vim.opt.foldmethod = "expr"
+  vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+end
+
