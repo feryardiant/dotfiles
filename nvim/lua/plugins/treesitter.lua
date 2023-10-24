0f51936c38eacece745e1a246b099ce66ada9bec
Fery Wardiyanto <ferywardiyanto@gmail.com>
2023-10-25 04:56:09 +0700

    chore(nvim): workaround with treesitter config
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,18 +2,22 @@ return {
 
   {
     'nvim-treesitter/nvim-treesitter',
+    version = false,
+    build = ':TSUpdate',
+    -- cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
     dependencies = {
       { 'nvim-treesitter/playground' },
-      { 'windwp/nvim-ts-autotag' },
-      { 'nvim-treesitter/nvim-treesitter-context', opts = { enable = true } }
+      -- {
+      --   'nvim-treesitter/nvim-treesitter-textobjects',
+      -- },
     },
-    config = function()
-      require('nvim-treesitter.configs').setup({
+    opts = {
+        indent = { enable = true },
+        highlight = { enable = true },
         -- https://github.com/windwp/nvim-ts-autotag
         autotag = {
           enable = true,
         },
-
         -- A list of parser names
         ensure_installed = {
           'html',
@@ -27,17 +31,27 @@ return {
           'vue',
           'yaml'
         },
-
         -- Install parsers asynchronously
         sync_install = false,
-
         -- Auto install missing parsers when entering buffer
         auto_install = true,
-
-        indent = { enable = true },
-        highlight = { enable = true }
-      })
+    },
+    config = function(_, opts)
+      require('nvim-treesitter.configs').setup(opts)
     end
+  },
+
+  -- Show context of the current function
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    enable = true,
+    opts = { mode = 'cursor', max_line = 3 }
+  },
+
+  -- Automatically add closing tags for HTML and JSX
+  {
+    'windwp/nvim-ts-autotag',
+    opts = {}
   },
 
 }
