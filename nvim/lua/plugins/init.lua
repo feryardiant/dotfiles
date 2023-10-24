a964ba4128f430d6b96594a10762341e69c786f8
Fery Wardiyanto <ferywardiyanto@gmail.com>
2023-10-24 17:05:40 +0700

    chore(nvim): initialize my favorite color scheme
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,32 @@
+return {
+  { 'folke/lazy.nvim', version = 'stable' },
+
+  {
+    'Shatur/neovim-ayu', name = 'ayu',
+    lazy = false,
+    priority = 1000,
+    config = function ()
+      local colors = require('ayu.colors')
+      local ayu = require('ayu')
+
+      colors.generate()
+
+      ayu.setup({
+        overrides = {
+          Normal = { bg = 'none' },
+          NormalFloat = { bg = '#3a3a3a' },
+          SignColumn = { bg = 'none' },
+          Comment = { fg = colors.comment },
+          CursorLine = { bg = '#1c1c1c' },
+          CursorLineNr = { bg = '#1c1c1c', fg = colors.accent },
+          ColorColumn = { bg = '#262626' },
+          Pmenu = { bg = colors.selection_inactive },
+          PmenuSel = { bg = colors.selection_bg },
+          Visual = { bg = colors.selection_bg },
+        }
+      })
+
+      ayu.colorscheme()
+    end
+  }
+}
