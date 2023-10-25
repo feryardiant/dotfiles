6abceb3b5ac27d42db2cd662f88ecee341b7680a
Fery Wardiyanto <ferywardiyanto@gmail.com>
2023-10-25 08:47:58 +0700

    chore(nvim): messing around with telescope configs
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,26 +2,52 @@ return {
 
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.4',
+    cmd = 'Telescope',
+    version = false,
     dependencies = {
-      'nvim-lua/plenary.nvim'
+      { 'nvim-lua/plenary.nvim' },
+      {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'make',
+        enabled = vim.fn.executable('make') == 1,
+      }
     },
-    config = function()
-      local builtin = require('telescope.builtin')
-      local telescope = require('telescope')
+    opts = function ()
+      local actions = require('telescope.actions')
 
-      vim.keymap.set('n', '<leader><space>', builtin.buffers, { desc = 'Find files from existing buffer' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Find files inside git repository' })
-      vim.keymap.set('n', '<leader>ds', builtin.lsp_document_symbols, { desc = 'Find [D]ocument [S]ymbols' })
-
-      telescope.setup({
+      return {
         defaults = {
+          prompt_prefix = " ",
+          selection_caret = " ",
           layout_config = {
             vertical = { width = 0.5 }
+          },
+          mappings = {
+            i = {
+              ['<Esc>'] = actions.close
+            },
+            n = {
+              ['<Esc>'] = actions.close
+            },
           }
-        }
-      })
+        },
+      }
+    end,
+    init = function ()
+      local builtin = require('telescope.builtin')
+
+      vim.keymap.set('n', '<leader><space>', builtin.buffers, { desc = 'Find files from existing buffer' })
+      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = 'Search files' })
+      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = 'Search current word' })
+      vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Find files inside git repository' })
+      vim.keymap.set('n', '<leader>ds', builtin.lsp_document_symbols, { desc = 'Find document symbols' })
+    end,
+    config = function(_, opts)
+      local telescope = require('telescope')
+
+      telescope.setup(opts)
+
+      telescope.load_extension('fzf')
     end
   },
 
