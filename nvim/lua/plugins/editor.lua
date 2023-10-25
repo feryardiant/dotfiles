0b77f848afec6759f82fb5098a89ffc1df441ede
Fery Wardiyanto <ferywardiyanto@gmail.com>
2023-10-25 10:58:06 +0700

    feat(nvim): replacing `numToStr/Comment.nvim` with `echasnovski/mini.comment` and `JoosepAlviste/nvim-ts-context-commentstring`
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -19,9 +19,21 @@ return {
   },
 
   {
-    'numToStr/Comment.nvim',
-    lazy = false,
-    opts = {},
+    'echasnovski/mini.comment',
+    opts = {
+      options = {
+        custom_commentstring = function()
+          return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
+        end,
+      },
+    },
+  },
+
+  {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    opts = {
+      enable_autocmd = false,
+    },
   },
 
   {
