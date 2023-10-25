d500a39fee4e7ff9811e59249088afe624e856da
Fery Wardiyanto <ferywardiyanto@gmail.com>
2023-10-25 11:13:36 +0700

    feat(nvim): initialize `echasnovski/mini.surround`
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -8,7 +8,7 @@ return {
     opts = {
       current_line_blame = true,
       signs = {
-        untracked = { text = "│" },
+        untracked = { text = '│' },
       }
     }
   },
@@ -19,18 +19,51 @@ return {
   },
 
   {
+    'echasnovski/mini.surround',
+    keys = function(_, keys)
+      -- Populate the keys based on the user's options
+      local plugin = require('lazy.core.config').spec.plugins['mini.surround']
+      local opts = require('lazy.core.plugin').values(plugin, 'opts', false)
+      local mappings = {
+        { opts.mappings.add, desc = 'Add surrounding', mode = { 'n', 'v' } },
+        { opts.mappings.delete, desc = 'Delete surrounding' },
+        { opts.mappings.find, desc = 'Find right surrounding' },
+        { opts.mappings.find_left, desc = 'Find left surrounding' },
+        { opts.mappings.highlight, desc = 'Highlight surrounding' },
+        { opts.mappings.replace, desc = 'Replace surrounding' },
+        { opts.mappings.update_n_lines, desc = 'Update `MiniSurround.config.n_lines`' },
+      }
+      mappings = vim.tbl_filter(function(m)
+        return m[1] and #m[1] > 0
+      end, mappings)
+      return vim.list_extend(mappings, keys)
+    end,
+    opts = {
+      mappings = {
+        add = 'sa', -- Add surrounding in Normal and Visual modes
+        delete = 'sd', -- Delete surrounding
+        find = 'sf', -- Find surrounding (to the right)
+        find_left = 'sF', -- Find surrounding (to the left)
+        highlight = 'sh', -- Highlight surrounding
+        replace = 'sr', -- Replace surrounding
+        update_n_lines = 'sn', -- Update `n_lines`
+      },
+    },
+  },
+
+  {
     'echasnovski/mini.comment',
     opts = {
       options = {
         custom_commentstring = function()
-          return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
+          return require('ts_context_commentstring.internal').calculate_commentstring() or vim.bo.commentstring
         end,
       },
     },
   },
 
   {
-    "JoosepAlviste/nvim-ts-context-commentstring",
+    'JoosepAlviste/nvim-ts-context-commentstring',
     opts = {
       enable_autocmd = false,
     },
