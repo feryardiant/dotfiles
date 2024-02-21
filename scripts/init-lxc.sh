8e971dca88343c240e3398ea59138e10a455765b
Fery Wardiyanto <ferywardiyanto@gmail.com>
2024-02-21 13:26:12 +0700

    feat: add initialization script for lxc container
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

---
+++ scripts/init-lxc.sh
@@ -0,0 +1,96 @@
+#!/usr/bin/env bash
+
+{
+# clear any previous sudo permission
+sudo -k
+
+LANG="id_ID.UTF-8"
+
+# run inside sudo
+sudo sh <<SCRIPT
+printf ' - '
+locale-gen $LANG > /dev/null 2>&1
+update-locale LC_ALL="$LANG" LANG="$LANG"
+dpkg-reconfigure --frontend noninteractive locales
+ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
+dpkg-reconfigure --frontend noninteractive tzdata
+printf ' done\n'
+
+printf ' - Check for updates...'
+apt update -qq > /dev/null 2>&1
+printf ' done\n'
+
+printf ' - Update packages...'
+apt dist-upgrade -qqy > /dev/null 2>&1
+printf ' done\n'
+
+printf ' - Install basic tools...'
+apt install -qqy --no-install-recommends vim htop tree curl net-tools git unzip zip > /dev/null 2>&1
+printf ' done\n'
+
+printf ' - '
+update-alternatives --set editor /usr/bin/vim.basic
+printf ' done\n'
+
+printf ' - '
+adduser --disabled-password --gecos "" admin
+echo "admin:password" | chpasswd
+usermod -aG adm,root,sudo,www-data admin
+echo "admin ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/90-admin-users
+mkdir /home/admin/.ssh
+cat .ssh/authorized_keys > /home/admin/.ssh/authorized_keys
+chown -R admin:admin /home/admin/.ssh
+chmod 700 /home/admin/.ssh
+chmod 600 /home/admin/.ssh/*
+printf ' done\n'
+
+printf ' - Update default SSH configs...'
+sed -iE "s~#PermitRootLogin .*~PermitRootLogin prohibit-password~" /etc/ssh/sshd_config
+sed -iE "s~#PubkeyAuthentication .*~PubkeyAuthentication yes~" /etc/ssh/sshd_config
+sed -iE "s~#PasswordAuthentication .*~PasswordAuthentication no~" /etc/ssh/sshd_config
+sed -iE "s~#AllowAgentForwarding .*~AllowAgentForwarding yes~" /etc/ssh/sshd_config
+sed -iE "s~#AllowTcpForwarding .*~AllowTcpForwarding yes~" /etc/ssh/sshd_config
+sed -iE "s~#PermitTTY .*~PermitTTY yes~" /etc/ssh/sshd_config
+systemctl start sshd
+printf ' done\n'
+
+printf ' - Update default VIM configs...'
+cat >> /etc/vim/vimrc.local<< VIMRC
+set nocompatible
+filetype plugin indent on
+
+set encoding=utf-8 nobomb  " BOM often causes trouble
+set mouse=a                " Enable moouse in all in all modes
+set noerrorbells           " Disable error bells
+set ffs=unix,dos,mac       " Use Unix as the standard file type
+set nohidden               " Close the buffer when tab is closed
+set confirm                " Confirm before exit if file has changed
+set nu relativenumber      " Enable line numbers
+
+if has('wildmenu')
+	set wildmenu
+endif
+
+set t_Co=256
+hi! Comment ctermfg=240
+hi! CursorLineNr ctermfg=255
+hi! LineNr ctermfg=240
+hi! StatusLine ctermbg=238
+hi! Visual ctermbg=238
+
+set wildmode=longest:full,full
+set wildchar=<TAB>
+
+" Searches
+set hlsearch    " Highlight searches
+set incsearch   " Highlight dynamically as pattern is typed
+set ignorecase  " Ignore case of searches
+set smartcase   " Ignore 'ignorecase' if search patter contains uppercase characters
+set wrapscan    " Searches wrap around end of file
+VIMRC
+
+printf ' done\n'
+SCRIPT
+
+printf 'All done\n'
+}
