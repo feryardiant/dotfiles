acbc4750698a07d64d81d2bd9c05aa467acc745c
Fery Wardiyanto <ferywardiyanto@gmail.com>
2024-09-10 09:59:30 +0700

    chore(nvim): get rid of lzp-zero configs
    but still can't get `vue` + `ts` integration to work
    
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -85,7 +85,7 @@ return {
       { 'williamboman/mason.nvim', config = true },
       { 'williamboman/mason-lspconfig.nvim' },
       { 'nvim-cmp' },
-      -- { 'hrsh7th/cmp-nvim-lsp' }
+      { 'hrsh7th/cmp-nvim-lsp' }
     },
     config = function ()
       vim.api.nvim_create_autocmd('LspAttach', {
@@ -114,30 +114,78 @@ return {
       })
 
       local capabilities = vim.lsp.protocol.make_client_capabilities()
-      -- capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+      capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
       require('mason').setup({})
+
+      local lspconfig = require('lspconfig')
 
       local servers = {
         nginx_language_server = {},
 
         emmet_ls = {},
         html = {},
-        cssls = {},
+        cssls = {
+          settings = {
+            css = {
+              lint = {
+                -- Do not warn for Tailwind's @apply rule
+                unknownAtRules = 'ignore',
+              },
+            },
+          },
+        },
         tailwindcss = {},
 
         intelephense = {},
         eslint = {},
-        tsserver = {},
+        tsserver = {
+          filetypes = {
+            'javascript',
+            'javascriptreact',
+            'javascript.jsx',
+            'typescript',
+            'typescriptreact',
+            'typescript.tsx',
+            'vue',
+          },
+          init_options = {
+            plugins = {
+              {
+                name = "@vue/typescript-plugin",
+                location = require('mason-registry')
+                  .get_package('vue-language-server')
+                  :get_install_path()
+                  .. '/node_modules/@vue/language-server'
+                  .. '/node_modules/@vue/typescript-plugin',
+                languages = { 'vue', 'javascript', 'typescript' }
+              },
+            }
+          },
+          settings = {
+            tsserver_plugins = {
+              '@vue/typescript-plugin',
+            },
+          }
+        },
         svelte = {},
-        volar = {},
+        volar = {
+          init_options = {
+            tsdk = '/opt/homebrew/lib/node_modules/typescript/lib'
+          },
+        },
 
-        jsonls = {},
-        yamlls = {},
+        jsonls = {
+          settings = {
+            json = require('schemas').json,
+          },
+        },
+        yamlls = {
+          settings = {
+            yaml = require('schemas').yaml,
+          },
+        },
         lua_ls = {
-          -- cmd = {...},
-          -- filetypes = { ...},
-          -- capabilities = {},
           settings = {
             Lua = {
               completion = {
@@ -167,137 +215,11 @@ return {
 
             config.capabilities = vim.tbl_deep_extend('force', {}, capabilities, config.capabilities or {})
 
-            require('lspconfig')[server_name].setup(config)
+            lspconfig[server_name].setup(config)
           end,
         }
       })
     end
   }
-
-  -- {
-  --   'VonHeikemen/lsp-zero.nvim',
-  --   branch = 'v3.x',
-  --   event = { 'BufReadPre', 'BufNewFile' },
-  --   dependencies = {
-  --     { 'neovim/nvim-lspconfig' },
-  --     { 'williamboman/mason.nvim' },
-  --     { 'williamboman/mason-lspconfig.nvim' },
-  --     { 'nvim-cmp' },
-  --   },
-  --   init = function()
-  --     vim.diagnostic.config({
-  --       virtual_text = true
-  --     })
-  --   end,
-  --   config = function()
-  --     local lsp_zero = require('lsp-zero')
-  --     local mason_lspconfig = require('mason-lspconfig')
-
-  --     require('mason').setup({})
-
-  --     local nmap = function(buffer, keys, func, desc)
-  --       if desc then
-  --         desc = 'LSP: ' .. desc
-  --       end
-  --       vim.keymap.set('n', keys, func, { buffer = buffer, desc = desc, remap = false })
-  --     end
-
-  --     mason_lspconfig.setup({
-  --       ensure_installed = {
-  --         'emmet_ls',
-  --         'jsonls',
-  --         'html',
-  --         'cssls',
-  --         'tailwindcss',
-  --         'svelte',
-  --         'lua_ls',
-  --         'eslint',
-  --         'volar',
-  --       },
-  --       handlers = {
-  --         -- lsp_zero.default_setup,
-
-  --         function (server_name)
-  --           require('lspconfig')[server_name].setup({})
-  --         end,
-
-  --         -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#volar
-  --         volar = function()
-  --           local util = require('lspconfig.util')
-  --           local global_lib = '/opt/homebrew/lib/node_modules/typescript/lib'
-
-  --           local function get_ts_server_path(root_dir)
-  --             local found_lib = ''
-  --             local function check_dir(path)
-  --               found_lib = util.path.join(path, 'node_modules', 'typescript', 'lib')
-  --               if util.path.exists(found_lib) then
-  --                 return path
-  --               end
-  --             end
-
-  --             if util.search_ancestors(root_dir, check_dir) then
-  --               return found_lib
-  --             else
-  --               return global_lib
-  --             end
-  --           end
-
-  --           require('lspconfig').volar.setup({
-  --             filetypes = { 'vue', 'typescript', 'javascript' },
-  --             init_options = {
-  --               tsdk = '/opt/homebrew/lib/node_modules/typescript/lib'
-  --             },
-  --             on_new_config = function(new_config, new_root_dir)
-  --               new_config.init_options.typescript.tsdk = get_ts_server_path(new_root_dir)
-  --             end,
-  --           })
-  --         end,
-
-  --         tsserver = function()
-  --           local lspconfig = require('lspconfig')
-  --           local vue_typescript_plugin = require('mason-registry')
-  --             .get_package('vue-language-server')
-  --             :get_install_path()
-  --             .. '/node_modules/@vue/language-server'
-  --             .. '/node_modules/@vue/typescript-plugin'
-
-  --           lspconfig.ts_ls.setup({
-  --             init_options = {
-  --               plugins = {
-  --                 {
-  --                   name = "@vue/typescript-plugin",
-  --                   location = vue_typescript_plugin,
-  --                   languages = {'javascript', 'typescript', 'vue'}
-  --                 },
-  --               }
-  --             },
-  --             root_dir = lspconfig.util.root_pattern({ "package.json", "node_modules" }),
-  --             filetypes = {
-  --               'javascript',
-  --               'javascriptreact',
-  --               'javascript.jsx',
-  --               'typescript',
-  --               'typescriptreact',
-  --               'typescript.tsx',
-  --               'vue',
-  --             },
-  --           })
-  --         end,
-
-  --         lua_ls = function()
-  --           require('lspconfig').lua_ls.setup(lsp_zero.nvim_lua_ls())
-  --         end,
-  --       }
-  --     })
-
-  --     lsp_zero.on_attach(function(_, buffer)
-  --       lsp_zero.default_keymaps({ buffer = buffer })
-
-  --       nmap(buffer, '<leader>ca', vim.lsp.buf.code_action, 'Code Action')
-  --     end)
-
-  --     lsp_zero.setup_servers({})
-  --   end
-  -- },
 
 }
---
+++ nvim/lua/schemas/init.lua
@@ -0,0 +1,40 @@
+return {
+  json = {
+    schemas = {
+      {
+        fileMatch = { 'composer.json' },
+        url = 'https://json.schemastore.org/composer'
+      },
+      {
+        fileMatch = { 'jsconfig.json' },
+        url = 'https://json.schemastore.org/jsconfig'
+      },
+      {
+        fileMatch = { 'tsconfig.json' },
+        url = 'https://json.schemastore.org/tsconfig'
+      },
+      {
+        fileMatch = { 'package.json' },
+        url = 'https://json.schemastore.org/package'
+      },
+      {
+        fileMatch = { '.prettierrc.json', '.prettierrc' },
+        url = 'https://json.schemastore.org/prettierrc.json',
+      },
+      {
+        fileMatch = { '.eslintrc.json' },
+        url = 'https://json.schemastore.org/eslintrc.json'
+      },
+    },
+  },
+
+  yaml = {
+    schemas = {
+      ['https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json'] = '.gitlab-ci.yml',
+      ['http://json.schemastore.org/composer'] = 'composer.yaml',
+      ['https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json'] = 'docker-compose*.yml',
+      ['https://raw.githubusercontent.com/kamilkisiela/graphql-config/v3.0.3/config-schema.json'] = '.graphqlrc*',
+      ['https://json.schemastore.org/github-workflow.json'] = '.github/workflow/*.yml',
+    },
+  },
+}
