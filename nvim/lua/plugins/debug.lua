401643d706e041316947cef51af429adcaefdd3c
Fery Wardiyanto <ferywardiyanto@gmail.com>
2024-09-10 17:41:55 +0700

    chore(nvim): make php debugger to work
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- nvim/lua/plugins/debug.lua
+++ nvim/lua/plugins/debug.lua
@@ -26,6 +26,19 @@ return {
       local dap = require('dap')
       local dapui = require('dapui')
 
+      ---Retrieve mason package install path
+      ---@param package_name string
+      ---@param path string
+      ---@return string
+      local function get_mason_pkg_path (package_name, path)
+        local util = require('lspconfig.util')
+
+        return util.path.join(
+          require('mason-registry').get_package(package_name):get_install_path(),
+          path
+        )
+      end
+
       require('mason-nvim-dap').setup({
         automatic_installation = true,
         ensure_installed = {
@@ -34,6 +47,27 @@ return {
           'php-debug-adapter',
           'js-debug-adapter',
           'node-debug2-adapter',
+        },
+        handlers = {
+          -- all sources with no handler get passed here
+          -- see https://github.com/jay-babu/mason-nvim-dap.nvim?tab=readme-ov-file#advanced-customization
+          function (config)
+            require('mason-nvim-dap').default_setup(config)
+          end,
+
+          -- https://github.com/mfussenegger/nvim-dap/wiki/Debug-Adapter-installation#php
+          ---@param config table
+          php = function (config)
+            config.adapters = {
+              type = 'executable',
+              command = 'node',
+              args = {
+                get_mason_pkg_path('php-debug-adapter', 'extension/out/phpDebug.js'),
+              }
+            }
+
+            require('mason-nvim-dap').default_setup(config)
+          end
         }
       })
 
