14d30fadc2d93757c1f67d663ce5131523ecbe93
Fery Wardiyanto <ferywardiyanto@gmail.com>
2024-09-10 12:46:47 +0700

    chore(nvim): remove `mason` setup
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -116,8 +116,6 @@ return {
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
-      require('mason').setup({})
-
       local lspconfig = require('lspconfig')
 
       local servers = {
