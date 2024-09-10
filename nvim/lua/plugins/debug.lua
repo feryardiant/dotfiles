1ac0854580020cfc2892779f38998b8e8945de60
Fery Wardiyanto <ferywardiyanto@gmail.com>
2024-09-10 15:19:28 +0700

    chore(nvim): add key binding
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- nvim/lua/plugins/debug.lua
+++ nvim/lua/plugins/debug.lua
@@ -8,20 +8,20 @@ return {
       'williamboman/mason.nvim',
       'jay-babu/mason-nvim-dap.nvim',
     },
-    -- keys = function (_, keys)
-    --   local dap = require('dap')
-    --   local dapui = require('dapui')
-    --
-    --   return {
-    --     { '<F5>', dap.continue, desc = 'Debug: Start/Continue' },
-    --     { '<F1>', dap.step_into, desc = 'Debug: Step into' },
-    --     { '<F2>', dap.step_over, desc = 'Debug: Step over' },
-    --     { '<F3>', dap.step_out, desc = 'Debug: Step out' },
-    --     { '<leader>b', dap.toggle_breakpoint, desc = 'Debug: Toggle Breakpoint' },
-    --     { '<F7>', dapui.toggle, desc = 'Debug: Toggle UI' },
-    --     unpack(keys)
-    --   }
-    -- end,
+    keys = function (_, keys)
+      local dap = require('dap')
+      local dapui = require('dapui')
+
+      return {
+        { '<F5>', dap.continue, desc = 'Debug: Start/Continue' },
+        { '<F1>', dap.step_into, desc = 'Debug: Step into' },
+        { '<F2>', dap.step_over, desc = 'Debug: Step over' },
+        { '<F3>', dap.step_out, desc = 'Debug: Step out' },
+        { '<leader>b', dap.toggle_breakpoint, desc = 'Debug: Toggle Breakpoint' },
+        { '<F7>', dapui.toggle, desc = 'Debug: Toggle UI' },
+        unpack(keys)
+      }
+    end,
     config = function ()
       local dap = require('dap')
       local dapui = require('dapui')
