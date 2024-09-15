b88d203bb9e29b687693f31a05234d19947f66ce
Fery Wardiyanto <ferywardiyanto@gmail.com>
2024-09-16 06:53:24 +0700

    chore(nvim): load luvit-meta library
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -5,16 +5,15 @@ return {
     -- used for completion, annotations and signatures of Neovim apis
     'folke/lazydev.nvim',
     ft = 'lua',
-    -- dependencies = {
-    --   { 'Bilal2453/luvit-meta', lazy = true },
-    -- },
     opts = {
-      -- library = {
-      --   -- Load luvit types when the `vim.uv` word is found
-      --   { path = 'luvit-meta/library', words = { 'vim%.uv' } },
-      -- },
+      library = {
+        -- Load luvit types when the `vim.uv` word is found
+        { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+      },
     },
   },
+
+  { 'Bilal2453/luvit-meta', lazy = true },
 
   {
     'lewis6991/gitsigns.nvim',
