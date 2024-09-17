06f268a5d571e121c2e9bd362c5d4b3abc1a237e
Fery Wardiyanto <ferywardiyanto@gmail.com>
2024-09-17 08:39:27 +0700

    chore(nvim): assign key binding to `trouble.nvim`
    Signed-off-by: Fery Wardiyanto <ferywardiyanto@gmail.com>

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -64,9 +64,17 @@ return {
 
   {
     'folke/trouble.nvim',
-    lazy = true,
+    cmd = 'Trouble',
     dependencies = {
       { 'nvim-tree/nvim-web-devicons' },
+    },
+    keys = {
+      { '<leader>xx', '<Cmd>Trouble diagnostics toggle<CR>', desc = '[Trouble] Diagnostics' },
+      { '<leader>xX', '<Cmd>Trouble diagnostics toggle filter.buf=0<CR>', desc = '[Trouble] Buffer diagnostics' },
+      { '<leader>xs', '<Cmd>Trouble symbols toggle focus=false<CR>', desc = '[Trouble] Symbols' },
+      { '<leader>xl', '<Cmd>Trouble lsp toggle focus=false win.position=right<CR>', desc = '[Trouble] LSP Definitions' },
+      { '<leader>xL', '<Cmd>Trouble loclist toggle<CR>', desc = '[Trouble] Location list' },
+      { '<leader>xq', '<Cmd>Trouble qflist toggle<CR>', desc = '[Trouble] Quickfix list' },
     },
     opts = {
       use_diagnostic_signs = true
